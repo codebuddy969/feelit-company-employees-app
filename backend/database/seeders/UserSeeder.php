21b9c491bef7f3a0ed4dbaad7a05163f2fb074e2
vladimir_secrier <vladimir.secrier@codefactorygroup.com>
2023-03-17 20:28:53 +0200

    Build React image

---
+++ backend/database/seeders/UserSeeder.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
+use App\Models\User;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        $users = [
+            [
+                'name' => 'Vlad Secrier',
+                'email' => 'admin@admin.com',
+                'password' => Hash::make('password'),
+            ],
+            [
+                'name' => 'Vlad Secrier',
+                'email' => 'vlad@mail.com',
+                'password' => Hash::make('password'),
+            ]
+        ];
+
+        foreach ($users as $user) {
+            User::create($user);
+        }
+    }
+}
